8b14b245d65112ff54ab2e450eecbe2c7e64c0dd
David Ogungbemi <daveedog2003@gmail.com>
2024-11-19 16:46:39 +0100

    update

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -31,11 +31,11 @@ const ProjectCard = ({ img, title, description }) => {
           className="w-full scale-110 transition-all duration-700 object-cover h-full max-h-[270px]"
         />
       )}
-      <div className="media-content bg-gradient-to-t from-blue via-lightblue to-transparent text-white absolute -bottom-[10rem] left-0 transition-all duration-300 w-full pt-[0.5rem] md:pt-[1.5rem]">
-        <h3 className="media-title text-[1.5rem] font-jakarta px-[1rem] pb-[0.5rem] md:pb-[1rem]">
+      <div className="media-content bg-gradient-to-t from-textblack via-blackish to-blackish text-white absolute inset-0 transition-all duration-300 w-full pt-[0.5rem] md:pt-[1.5rem] opacity-0">
+        <h3 className="media-title text-[1.5rem] font-jakarta px-[1rem] pb-[0.5rem] md:pb-[1rem] absolute -bottom-[64px] ">
           {title}
         </h3>
-        <p className="media-description text-base font-inter px-[1rem] pb-[0.5rem] md:pb-[1rem]">
+        <p className="media-description text-base font-inter px-[1rem] pb-[0.5rem] md:pb-[1rem] absolute -bottom-[64px]">
           {description}
         </p>
       </div>
